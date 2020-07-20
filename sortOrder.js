334a4ac4a3b8d79084c63b34ccc057dd3c72a3b9
DJ Viking <anders.swedishviking@gmail.com>
2020-07-20 09:33:17 -0400

    sortOrder.js has been updated I think. so changed it around

--- sortOrder.js
+++ sortOrder.js
@@ -1,21 +1,36 @@
 const argumentWord1 = process.argv[2];
 const argumentWord2 = process.argv[3];
 
+const lowarg1 = argumentWord1.toLowerCase();
+const lowarg2 = argumentWord2.toLowerCase();
 
-compareWords(argumentWord1, argumentWord2);
-
-function compareWords(arg1, arg2){
-
-    var lowarg1 = arg1.toLowerCase();
-    var lowarg2 = arg2.toLowerCase();
-
-    if (lowarg1 < lowarg2){
-        return console.log("-1");
-    }
-    if (lowarg1 === lowarg2){
-        return console.log("0");
-    }
-    if (lowarg1 > lowarg2){
-        return console.log("1");
-    }
+if (lowarg1 < lowarg2){
+    console.log("-1");
 }
+else if (lowarg1 === lowarg2){
+    console.log("0");
+}
+else if (lowarg1 > lowarg2){
+    console.log("1");
+}
+
+
+// compareWords(argumentWord1, argumentWord2);
+
+// function compareWords(arg1, arg2){
+
+//     let lowarg1 = arg1.toLowerCase();
+//     console.log(lowarg1);
+//     let lowarg2 = arg2.toLowerCase();
+//     console.log(lowarg2);
+
+//     if (lowarg1 < lowarg2){
+//         return console.log("-1");
+//     }
+//     if (lowarg1 === lowarg2){
+//         return console.log("0");
+//     }
+//     if (lowarg1 > lowarg2){
+//         return console.log("1");
+//     }
+// }
